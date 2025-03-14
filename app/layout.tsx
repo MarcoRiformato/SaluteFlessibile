056b7478cadca1e89c8e0545f3d35d0ebd93a490
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2025-03-14 08:50:00 +0100

    Adding hothar tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "FlexiCare - Trova il tuo specialista di fiducia",
   description:
     "Prenota visite specialistiche, consulenze e trattamenti nella più grande rete di professionisti in Italia",
-    generator: 'v0.dev'
+  generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -16,11 +17,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="it">
+      <head>
+        {/* Hotjar Tracking Code */}
+        <Script id="hotjar-tracking" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5332577,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
 }
-
-
-
-import './globals.css'
