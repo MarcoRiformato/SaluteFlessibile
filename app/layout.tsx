ae145ef300cf7efa70c8025430cf3bcb623f860e
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2025-04-01 12:18:17 +0200

    Add Google Tag Manager integration to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,16 @@ export default function RootLayout({
   return (
     <html lang="it">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N823N84X');
+          `}
+        </Script>
         <Script id="hotjar" strategy="afterInteractive">
           {`
             (function(h,o,t,j,a,r){
@@ -36,6 +46,11 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="overflow-x-hidden">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N823N84X"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
         {children}
         <IubendaScripts />
         <MetaPixel />
